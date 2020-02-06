c4b689d9a294f62a688f10c3c12588b0e2da8c53
doctor-glitch <harinrajan2014@gmail.com>
2020-02-06 15:41:22 +0530

    calculations implemented

--- index.js
+++ index.js
@@ -81,6 +81,117 @@ app.post("/login", function (req, res) {
   })
 });
 
+app.post("/checkout",auth, function (req, res) {
+  finalPrice = req.body.price;
+  signUpDiscount = 0;
+  referalDiscount = 0;
+  cashbackDiscount = 0;
+  refundsDiscount = 0;
+  referalCommDiscount = 0;
+  SalesCommDiscount = 0;
+
+  mainBalance = req.session.user.cashback + req.session.user.refunds + req.session.user.referalComm + req.session.user.SalesComm;
+  //calculate signupDiscount
+  if (req.body.signUp == true) {
+    signUpDiscount = (0.1 * req.body.price)
+    if (req.session.user.signupCredit < signUpDiscount) {
+      signUpDiscount = req.session.user.signupCredit;
+    }
+    finalPrice = req.body.price - signUpDiscount;
+  }
+
+  //calculate referalDiscount
+  if (req.body.referal == true) {
+    // referalDiscount = req.session.user.referalCredit;
+    // else if (referalDiscount >= finalPrice) {
+    //   referalDiscount = referalDiscount - finalPrice;
+    //   finalPrice = 0;
+    // } else {
+    //   finalPrice = finalPrice - referalDiscount;
+    //   referalDiscount = req.session.user.referalCredit - finalPrice;
+    // }
+    if (finalPrice >= req.session.user.referalCredit && req.session.user.referalCredit != 0) {
+      referalDiscount = req.session.user.referalCredit;
+      finalPrice = finalPrice - referalDiscount;
+    } else {
+      referalDiscount = req.session.user.referalCredit - finalPrice;
+      finalPrice = 0;
+    }
+  }
+
+  //deduct from main balance
+  if (req.body.mainBal == true) {
+    if (mainBalance == 0) {
+      console.log("check1");
+      return res.json({
+        finalPrice,
+        signUpDiscount,
+        referalDiscount,
+        cashbackDiscount,
+        refundsDiscount,
+        referalCommDiscount,
+        SalesCommDiscount,
+      });
+    } else {
+      //cashback discount
+      if (finalPrice >= req.session.user.cashback && req.session.user.cashback != 0) {
+        cashbackDiscount = req.session.user.cashback;
+        finalPrice = finalPrice - cashbackDiscount;
+      } else {
+        cashbackDiscount = req.session.user.cashback - finalPrice;
+        finalPrice = 0;
+      }
+
+      //refunds discount
+      if (finalPrice >= req.session.user.refunds && req.session.user.refunds != 0) {
+        refundsDiscount = req.session.user.refunds;
+        finalPrice = finalPrice - refundsDiscount;
+      } else {
+        refundsDiscount = req.session.user.refunds - finalPrice;
+        finalPrice = 0;
+      }
+
+      //referalComm discount
+      if (finalPrice >= req.session.user.referalComm && req.session.user.referalComm != 0) {
+        referalCommDiscount = req.session.user.referalComm;
+        finalPrice = finalPrice - referalCommDiscount;
+      } else {
+        referalCommDiscount = req.session.user.referalComm - finalPrice;
+        finalPrice = 0;
+      }
+
+      //SalesComm discount
+      if (finalPrice >= req.session.user.SalesComm && req.session.user.SalesComm != 0) {
+        SalesCommDiscount = req.session.user.SalesComm;
+        finalPrice = finalPrice - SalesCommDiscount;
+      } else {
+        SalesCommDiscount = req.session.user.SalesComm - finalPrice;
+        finalPrice = 0;
+      }
+      console.log("check2");
+      return res.json({
+        finalPrice,
+        signUpDiscount,
+        referalDiscount,
+        cashbackDiscount,
+        refundsDiscount,
+        referalCommDiscount,
+        SalesCommDiscount,
+      });
+    }//end of else
+  }
+  console.log("check3");
+  return res.json({
+    finalPrice,
+    signUpDiscount,
+    referalDiscount,
+    cashbackDiscount,
+    refundsDiscount,
+    referalCommDiscount,
+    SalesCommDiscount,
+  });
+});
+
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
